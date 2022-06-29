608587d023486f67140fa66074b3fc7d2885c137
Almont michel <michel.almont@gmail.com>
2022-06-29 18:28:58 -0400

    playRandom

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const newGame=document.getElementById('newGameIcon');
 const play=document.getElementById('play');
+const save=document.getElementById('save');
 const changeColors=document.querySelector('.NewGame');
 const scoreFinalsChangePlayer1=document.getElementById('scoreFinalPlayer1');
 const scoreChangePlayer1=document.getElementById('scorePlayer1');
 const scoreFinalsChangePlayer2=document.getElementById('scoreFinalPlayer2');
 const scoreChangePlayer2=document.getElementById('scorePlayer2');
 let playPlayer1=document.querySelector('.playGamePlayer1');
+let playPlayer2=document.querySelector('.playGamePlayer2');
 let staticDice=document.querySelector('#staticDice');
 let animeDice=document.querySelector('#animeDice');
-
+let choicePlayerBool;
 let scoreFinalPlayer1;
 let scorePlayer1;
 let scoreFinalPlayer2;
 let scorePlayer2;
 
+// startGame
 newGame.addEventListener('click', startGame);
 
-play.addEventListener('click', playGameAnime);
+// push playDice
+play.addEventListener('click', playRandom);
+
+// push saveScoreFinal
+save.addEventListener('click', saveScoreFinal);
+
+
 // Start game
 
 function startGame(){
-    // console.log("test");
+    choicePlayerBool=true;
     scoreFinalPlayer1=0;
     scorePlayer1=0;
     scoreFinalPlayer2=0;
@@ -33,9 +42,10 @@ function startGame(){
     scoreChangePlayer2.innerText=scorePlayer2.toString();
     playPlayer1.style.display = "block";
 
+
 }
 
-
+// Animation dice
 async function playGameAnime (){
 
     staticDice.style.display = "none";
@@ -43,17 +53,94 @@ async function playGameAnime (){
     await sleep(2000);
     playGamestatic();
 }
-function playGamestatic (){
 
+
+// desactive animation dice
+function playGamestatic (){
     animeDice.style.display = "none";
     staticDice.style.display = "inline";
-
 }
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
  }
 
- function numberAleatoire(min, max)
+//  return random numner
+ function playRandom(){
+    var score=numberRandom(1,4);
+    playGameAnime ()
+    choicePlayer(score);
+
+ }
+
+//  play dice
+ function numberRandom(min, max)
 {
  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// choice player
+function choicePlayer(scoreRandom){
+   
+
+    if( choicePlayerBool){
+
+        if(scoreRandom>1){
+            scorePlayer1=scorePlayer1+scoreRandom;
+            scoreChangePlayer1.innerText=scorePlayer1.toString();
+            analyseScore(scorePlayer1);
+
+        }else{
+            scoreFinalPlayer1=0;
+            scorePlayer1=0;
+            modifPlayerInterface();
+            scoreFinalsChangePlayer1.innerText=scoreFinalPlayer1.toString();
+            scoreChangePlayer1.innerText=scorePlayer1.toString();
+            choicePlayerBool=false;
+            // return;
+        }
+ 
+    }else{
+        if(scoreRandom>1){
+            scorePlayer2=scorePlayer2+scoreRandom;
+            scoreChangePlayer2.innerText=scorePlayer2.toString();
+            analyseScore(scorePlayer2);
+        }else{
+            scoreFinalPlayer2=0;
+            scorePlayer2=0;
+            modifPlayerInterface();
+            scoreFinalsChangePlayer2.innerText=scoreFinalPlayer2.toString();
+            scoreChangePlayer2.innerText=scorePlayer2.toString();
+            choicePlayerBool=true;
+            // return;
+
+        }
+       
+    }
+
+}
+
+function analyseScore(scoreCurrent){
+    if( scoreCurrent>100){
+        if(choicePlayerBool){
+            console.log("player 1 gagne")
+        }else{
+            console.log("player 2 gagne")
+        }
+    }
+}
+
+
+
+function modifPlayerInterface(){
+
+    if(choicePlayerBool){
+        changeColors.style.background = "linear-gradient(to right, white 50%, rgb(101, 90, 138) 50%)";
+        playPlayer1.style.display = "none";
+        playPlayer2.style.display = "block";
+    }else {
+        changeColors.style.background = "linear-gradient(to right, rgb(101, 90, 138) 50%, white 50%)";
+        playPlayer1.style.display = "block";
+        playPlayer2.style.display = "none";
+    }
 }
